3b2a76fd298ca1d5baabec40aa67a306cb87dc48
jake1234 <jakematthews072@gmail.com>
2019-05-07 14:57:03 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import pymongo
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+# mongodb information
+app = Flask(__name__)
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "Project0"
+COLLECTION_NAME = "Cluster0"
+
+# testing database connection
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+        
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
+
+
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
